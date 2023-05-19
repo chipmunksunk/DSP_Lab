1c8d1c86256489bc5c333f6d60b6a17863bbb840
chipmunksunk <chipmunksunk_h@hotmail.de>
2023-05-19 12:48:26 +0200

    test

--- lab3/DSPlab3_task_1.m
+++ lab3/DSPlab3_task_1.m
@@ -1,5 +1,5 @@
 close all
-clearvars
+clear all
 
 set(groot, 'defaultAxesFontSize',15)
 
@@ -12,29 +12,31 @@ for b=1:length(N_list)
     N = N_list(b);
     n = 0:N-1;
     t = (2*pi*f/fs*n);
-    dft = exp(-1i*2*pi / N .* n' * n);
+    dft_matrix = exp(-1i*2*pi / N .* n' * n);
     temp =  n' * n;
     x = sin(t);
-    tic
-    X = abs(dft * x');
-    time_matrix(b) = toc;
+    timerVal_mat = tic;
+    X = dft_matrix * x';
+    time_matrix(b) = toc(timerVal_mat);
+    X = abs(X);
 
-    figure
-    subplot(2,1,1)
-    f_plot = (0:1/N:1);
-    plot(f_plot(1:N/2),X(1:N/2))
-    xlabel('frequency in radians $(\times\pi)$', Interpreter='latex',FontSize=15)
-    ylabel('magnitude',FontSize=15)
-    title('DFT calculation via Matrix', FontSize=12)
-
-    subplot(2,1,2)
-    tic
-    X_fft = abs(fft(x));
-    time_fft(b) = toc;
-    plot(f_plot(1:N/2), X_fft(1:length(X_fft)/2))
-    xlabel('frequency in radians $(\times\pi)$', Interpreter='latex', FontSize=15)
-    ylabel('magnitude', FontSize=15)
-    title('DFT calculation via fft', FontSize=12)
+%     figure
+%     subplot(2,1,1)
+%     f_plot = (0:1/N:1);
+%     plot(f_plot(1:N/2),X(1:N/2))
+%     xlabel('frequency in radians $(\times\pi)$', Interpreter='latex',FontSize=15)
+%     ylabel('magnitude',FontSize=15)
+%     title('DFT calculation via Matrix', FontSize=12)
+% 
+%     subplot(2,1,2)
+    timerVal_fft = tic;
+    X_fft = fft(x);
+    time_fft(b) = toc(timerVal_fft);
+    X_fft = abs(X_fft);
+%     plot(f_plot(1:N/2), X_fft(1:length(X_fft)/2))
+%     xlabel('frequency in radians $(\times\pi)$', Interpreter='latex', FontSize=15)
+%     ylabel('magnitude', FontSize=15)
+%     title('DFT calculation via fft', FontSize=12)
 end
 
 %%
@@ -42,7 +44,7 @@ figure
 stem(N_list, time_matrix)
 hold on
 stem(N_list, time_fft)
-legend('matrix','fft', Location='northwest')
+legend('matrix','fft', Location='best')
 title('Comparison of computation time of DFT', FontSize=12)
 xlabel('signal length n')
 ylabel('time in s')
--- lab3/DSPlab3_task_3.m
+++ lab3/DSPlab3_task_3.m
@@ -16,8 +16,30 @@ ylabel('magnitude')
 title('DTFM signal of number "7"')
 
 %%
-s = dtmfcoef(X, fs);
-dtmf_chars_coded
+% s = dtmfcoef(X, fs);
+load('mynumber.mat', 'xx') % load encoded numbers into xx
+symb_length = length(xx)/11;
+dtmf_char = zeros([11 1]);
+for i = 1:11
+    symb = xx((i-1)*symb_length+1:i*symb_length);
+    symb_dtft = abs(fft(symb(1:256)));
+    symb_dtft = symb_dtft(1:length(symb_dtft)/2);
+%     plot(f, symb_dtft)
+%     pause;
+    dtmf_char(i) = dtmfcoef(symb_dtft, fs);
+end
+
+char(dtmf_char')
+%%
+figure
+plot(xx)
+title('Encoded DTMF signals')
+
+
+
+
+
+
 
 
 
