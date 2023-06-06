fe95f3ccd52cbf99460c2ffc8ba2306a87e5ebd4
Chrandloe <christoph-loeser@gmx.de>
2023-06-06 13:39:56 +0200

    Code from lab 3

--- lab3/DPSLab3_task_2.m
+++ lab3/DPSLab3_task_2.m
@@ -23,6 +23,10 @@ plot(w_1, hamming_1_DTFT_dB);
 hold off
 title("N = 36");
 legend(["Rectangular" "Bartlett" "Hamming"])
+xlim([0, pi/2]);
+ylim([-80,80]);
+xlabel("Normalized Frequency \omega");
+ylabel("Magnitude in dB");
 
 % N2
 rectangular_2 = rectwin(N_2);
@@ -45,6 +49,11 @@ plot(w_2, hamming_2_DTFT_dB);
 hold off
 title("N = 60");
 legend(["Rectangular" "Bartlett" "Hamming"])
+xlim([0, pi/2]);
+ylim([-80,80]);
+xlabel("Normalized Frequency \omega");
+ylabel("Magnitude in dB");
+
 
 %Look for main lobe and side lobe levels
 [r1_lobe_mag, r1_lobe_locs] = findpeaks(rectangular_1_DTFT_dB);
@@ -65,16 +74,28 @@ figure;
 plot(w_x, 20*log(abs(X_DTFT)));
 title("Sinusoid");
 xlim([0, pi/2]);
+ylim([-80,80]);
+xlabel("Normalized Frequency \omega");
+ylabel("Magnitude in dB");
 
 %Two sinusoids
-n = 0:N_1-1;
+n = 0:(10*N_1-1);
 w__0 = 0.2*pi;
-alpha = 0.1;
-w__1 = (w__0+2*pi*alpha)/N_1;
+alpha = 3;
+w__1 = w__0+(2*pi*alpha)/N_1;
 x_sins = sin(w__0*n)+sin(w__1*n);
-[X_sins_DTFT, w_xsins] = dtft(x_sins);
+x_sins_r = conv(x_sins, rectangular_1);
+x_sins_h = conv(x_sins, hamming_1);
+[X_sins_r_DTFT, w_r_xsins] = dtft(x_sins);
+[X_sins_h_DTFT, w_xsins_h] = dtft(x_sins_h);
 
 figure;
-plot(w_xsins, 20*log(abs(X_sins_DTFT)));
-title("Check resolution");
- xlim([0, pi/2]);
+plot(w_r_xsins, 20*log(abs(X_sins_r_DTFT)));
+hold on;
+plot(w_xsins_h, 20*log(abs(X_sins_h_DTFT)));
+title("Mixed Sinusoid");
+legend(["Rectangular" "Hamming"]);
+xlim([0, pi]);
+ylim([-120,150]);
+xlabel("Normalized Frequency \omega");
+ylabel("Magnitude in dB");
