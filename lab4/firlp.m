2aa198dabfea17bb85dd02ec7ef1a0590117ffd9
Chrandloe <christoph-loeser@gmx.de>
2023-06-06 15:13:37 +0200

    Task 1

--- lab4/firlp.m
+++ lab4/firlp.m
@@ -4,4 +4,4 @@ function [impulseResponse_h] = firlp(filterLength_N,cutoffFrequency_wc)
 alpha = (filterLength_N-1)/2;
 n = 0:filterLength_N-1;
 n = n-alpha;
-impulseResponse_h = sin(n.*cutoffFrequency_wc)./(n.*pi);
+impulseResponse_h = sinc(n.*cutoffFrequency_wc);
